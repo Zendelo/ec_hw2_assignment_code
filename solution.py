e4aa26660fee57bf45568c9137c68d850acb7c3b
Zendelo <olegzendel@campus.technion.ac.il>
2019-06-05 19:25:17 +0300

    create Student class

--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+import sys
+
 import pandas as pd
 
 
@@ -7,13 +9,14 @@ class Student:
         self.math_grade = None
         self.cs_grade = None
         self.pref_list = pref_list
-        self.max_rejects = len(pref_list)
+        self.max_rejects = len(pref_list) - 1
         self.project = None
         self.top_pref = None
         self.rejects = 0
 
     def assign_proj(self, proj_id):
         self.project = proj_id
+        free_students.discard(self.sid)
 
     def is_free(self):
         return bool(not self.project)
@@ -22,24 +25,38 @@ class Student:
         return self.top_pref
 
     def set_top_pref(self):
+        """Assuming {} can only appear at the end of the list of preferences"""
         if self.rejects < self.max_rejects:
-            temp = self.pref_list[self.rejects]
-            if type(temp) is int:
-                self.top_pref = temp
-            elif type(temp) is set:
-                self.top_pref = temp.pop()
+            self.top_pref = self.pref_list[self.rejects]
         else:
-            self.top_pref = self.pref_list #TODO: continue here
+            if self.pref_list:
+                temp = self.pref_list[self.rejects]
+                if type(temp) is set:
+                    try:
+                        self.top_pref = temp.pop()
+                    except KeyError:
+                        print('All possibilities depleted ! no Solution Found!')
+                        sys.exit(f'The program failed with student: {self.sid}')
+                else:
+                    print(f'Something strange with student :{self.sid}\n check his projects pref list')
+                    sys.exit(f'Something strange with student :{self.sid}\n check his projects pref list')
 
     def reject_student(self):
         self.project = None
+        free_students.add(self.sid)
         if self.rejects < self.max_rejects:
             self.rejects += 1
         self.set_top_pref()
 
+    def get_utility(self):
+        """We define the utility to be 10/(rejects+1), unless max_rejects reached, then it's -1"""
+        return 10.0 / (self.rejects + 1) if self.rejects < self.max_rejects else -1
+
 
 if __name__ == '__main__':
     students_dict = {}
-    df = pd.read_csv('data/train_data/students_pref_3.dat', index_col='student_id')
+    free_students = set()
+    df = pd.read_csv('data/train_data/students_pref_7.dat', index_col='student_id')
     for sid, pref_sr in df.iterrows():
         students_dict[sid] = Student(sid, list(pref_sr))
+        free_students.add(sid)
