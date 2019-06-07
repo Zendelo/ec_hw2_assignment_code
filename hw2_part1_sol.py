c7382b20cefbab2480617ab4aa0102587337a261
Oleg <olegzendel@campus.technion.ac.il>
2019-06-07 04:16:52 +0300

    submission - sent for review

--- hw2_part1_sol.py
+++ hw2_part1_sol.py
@@ -1,5 +1,4 @@
 import sys
-from collections import defaultdict
 
 import numpy as np
 import pandas as pd
@@ -21,7 +20,6 @@ class Student:
         self.utils = utils
         self.pair = None
         self.main_partner = True
-        self.values = utils
 
     def assign_proj(self, project):
         self.project = project
@@ -36,23 +34,7 @@ class Student:
         return self.top_pref
 
     def set_top_pref(self):
-        """Assuming {} can only appear at the end of the list of preferences"""
         self.top_pref = self.pref_list[self.rejects]
-
-    def update_values_by_prices(self, projects_dict):
-        for i, pid in enumerate(self.pref_list[:-1]):
-            self.values[i] = self.utils[i] - projects_dict[pid].price
-
-    def get_top_projects_by_util(self) -> set:
-        indices = set()
-        top_pids = set()
-        max_val = max(self.utils)
-        for j, val in enumerate(self.utils):
-            if val == max_val:
-                indices.add(j)
-        for j in indices:
-            top_pids.add(self.pref_list[j])
-        return top_pids
 
     def reject_student(self):
         self.project = None
@@ -74,14 +56,11 @@ class Student:
         return True if cur_pref > candidate_pref else False
 
     def get_utility(self):
-        if self.main_partner:
-            return self.utils[self.rejects]
-        else:
-            try:
-                idx = self.pref_list.index(self.project.pid)
-            except ValueError:
-                idx = self.max_rejects
-            return self.utils[idx]
+        try:
+            idx = self.pref_list.index(self.project.pid)
+        except ValueError:
+            idx = self.max_rejects
+        return self.utils[idx]
 
 
 class Project:
@@ -113,18 +92,6 @@ class Project:
 
     def add_offer(self, student):
         self.proposals[student.sid] = getattr(student, self.grade_type)
-
-    def raise_price(self):
-        self.price += 1
-
-
-def create_students_old(df, grades_df, utils_df):
-    students_dict = {}
-    for sid, pref_sr in df.iterrows():
-        students_dict[sid] = Student(sid, list(pref_sr), grades_df.loc[sid].math_grades, grades_df.loc[sid].cs_grades,
-                                     utils_df.loc[sid])
-        Student.free_students.add(sid)
-    return students_dict
 
 
 def create_students(df, grades_df):
@@ -174,27 +141,17 @@ def deferred_acceptance(students_dict, projects_dict):
         respond_offers(students_dict, projects_dict)
 
 
-def write_matching(matching: dict, task):
-    df = pd.DataFrame.from_dict(matching, orient='index')
-    df.to_csv(f'matching_task_{task}.csv', header=['pid'], index_label='sid')
-    print(f'\nmatching_task_{task}.csv was written\n')
-
-
 def task_one(pref_df, grades_df):
     students_dict = create_students(pref_df, grades_df)
     projects_dict = create_projects(pref_df)
     deferred_acceptance(students_dict, projects_dict)
-    total_welfare = 0
     matching = {}
     for sid, student in students_dict.items():
         if student.is_free():
-            print('wtf!')
+            print('Error! missing matching')
             sys.exit(sid)
         matching[sid] = student.project.pid
-        total_welfare += student.get_utility()
-    find_blocking_pairs(students_dict)
-    write_matching(matching, 1)
-    return total_welfare
+    return matching
 
 
 def merge_students(pref_df, grades_df, pairs_df: pd.DataFrame):
@@ -222,21 +179,16 @@ def task_two(pref_df, grades_df, pairs_df):
     students_dict = merge_students(pref_df, grades_df, pairs_df)
     projects_dict = create_projects(pref_df)
     deferred_acceptance(students_dict, projects_dict)
-    total_welfare = 0
     matching = {}
     for sid, student in students_dict.items():
         if student.is_free():
-            print('wtf!')
+            print('Error! missing matching')
             sys.exit(sid)
         matching[sid] = student.project.pid
-        total_welfare += student.get_utility()
-    find_blocking_pairs(students_dict)
-    write_matching(matching, 2)
-    return total_welfare
+    return matching
 
 
 def test_blocking(student, suspected_blocking, students_dict):
-    """If found blocking pair, both students are removed from the suspects set and return True"""
     cur_project = student.project
     counter = 0
     for suspect_id in suspected_blocking:
@@ -245,16 +197,13 @@ def test_blocking(student, suspected_blocking, students_dict):
             continue
         if cur_project.test_offer(suspect, student) and student.test_offer(suspect.project.pid):
             if suspect.test_offer(cur_project.pid) and suspect.project.test_offer(student, suspect):
-                # print('Blocking!')
-                # print(f'sid1: {student.sid} sid2: {suspect_id}')
-                # print(f'pid1: {student.project.pid} pid2: {suspect.project.pid}\n')
                 counter += 1
     return counter
 
 
 def find_blocking_pairs(students_dict):
     suspects = set()
-    # check if the main_student got his top priority, if not, might be blocking
+    # check if the student got his top priority, if not, might be blocking
     for sid, student in students_dict.items():
         if student.pref_list[0] == student.project.pid:
             continue
@@ -264,65 +213,76 @@ def find_blocking_pairs(students_dict):
     for sid in suspects_copy:
         counter += test_blocking(students_dict[sid], suspects, students_dict)
         suspects.remove(sid)
+    return counter
 
-    print(f'number of blocking pairs {counter}')
+
+def reconstruct_matching(students_dict, projects_dict, matching_df: pd.DataFrame):
+    for sid, pid in matching_df.itertuples():
+        student = students_dict[sid]
+        project = projects_dict[pid]
+        project.accept_offer(student)
 
 
-def update_students_values(students_dict, projects_dict):
+def reconstruct_matching_pairs(students_dict, projects_dict, matching_df):
+    for sid, pid in matching_df.itertuples():
+        student = students_dict[sid]
+        project = projects_dict[pid]
+        if student.main_partner:
+            project.accept_offer(student)
+        else:
+            project.partner_student = student
+            student.assign_proj(project)
+
+
+def recon_matching(matching_file, n):
+    grades_df = pd.read_csv(f'data/grades_{n}.csv', index_col='student_id')
+    pairs_df = pd.read_csv(f'data/pairs_{n}.csv', index_col=False, na_filter=True)
+    preferences_df = pd.read_csv(f'data/preferences_{n}.csv', index_col='student_id')
+    preferences_df.columns = preferences_df.columns.astype(int)
+    matching_df = pd.read_csv(matching_file, index_col='sid')
+    projects_dict = create_projects(preferences_df)
+    if 'coupled' in matching_file:
+        students_dict = merge_students(preferences_df, grades_df, pairs_df)
+        reconstruct_matching_pairs(students_dict, projects_dict, matching_df)
+    else:
+        students_dict = create_students(preferences_df, grades_df)
+        reconstruct_matching(students_dict, projects_dict, matching_df)
+    return students_dict, projects_dict
+
+
+def run_deferred_acceptance(n) -> dict:
+    grades_df = pd.read_csv(f'data/grades_{n}.csv', index_col='student_id')
+    preferences_df = pd.read_csv(f'data/preferences_{n}.csv', index_col='student_id')
+    preferences_df.columns = preferences_df.columns.astype(int)
+    matching_dict = task_one(preferences_df, grades_df)
+    # return {1: 2, 2: 3, 3: 4, 4: 1, 5: 5}
+    return matching_dict
+
+
+def run_deferred_acceptance_for_pairs(n) -> dict:
+    grades_df = pd.read_csv(f'data/grades_{n}.csv', index_col='student_id')
+    pairs_df = pd.read_csv(f'data/pairs_{n}.csv', index_col=False, na_filter=True)
+    preferences_df = pd.read_csv(f'data/preferences_{n}.csv', index_col='student_id')
+    preferences_df.columns = preferences_df.columns.astype(int)
+    matching_dict = task_two(preferences_df, grades_df, pairs_df)
+    # return {1: 2, 2: 2, 3: 3, 4: 3, 5: 5}
+    return matching_dict
+
+
+def count_blocking_pairs(matching_file, n) -> int:
+    students_dict, projects_dict = recon_matching(matching_file, n)
+    blocking_pairs = find_blocking_pairs(students_dict)
+    return blocking_pairs
+    # return 0 if '1' in matching_file else 1
+
+
+def calc_total_welfare(matching_file, n) -> int:
+    students_dict, projects_dict = recon_matching(matching_file, n)
+    total_welfare = 0
     for sid, student in students_dict.items():
-        student.update_values_by_prices(projects_dict)
-
-
-def make_edges(students_dict):
-    edges_st_pr = defaultdict(set)
-    edges_pr_st = defaultdict(set)
-    for sid, student in students_dict.items():
-        top_projs = student.get_top_projects_by_util()
-        edges_st_pr[sid].update(top_projs)
-        for pid in top_projs:
-            edges_pr_st[pid].add(sid)
-    return edges_st_pr, edges_pr_st
-
-
-def market_clearing(pref_df, grades_df, util_df):
-    students_dict = create_students(pref_df, grades_df, util_df)
-    projects_dict = create_projects(pref_df)
-
-    update_students_values(students_dict, projects_dict)
-    edges_st_pr, edges_pr_st = make_edges(students_dict)
-    constricted_set = set()
-    suspects = set()
-    for pid, st_set in edges_pr_st.items():
-        if len(st_set) > 1:
-            suspects.add(pid)
-            neighb = len(st_set)
-            for sid in st_set:
-                suspects.update(pid)
-    print(suspects)
-    print(edges_pr_st)
-    # TODO: need to implement BFS to look for matching and constricted sets
-    # print(len(edges_pr_st))
-    # print(edges_st_pr)
-    # print(len(edges_st_pr))
-
-
-def main(n):
-    print(f'running for dataset {n}')
-    grades_df = pd.read_csv(f'data/train_data/grades_{n}.csv', index_col='student_id')
-    # pref_df = pd.read_csv(f'data/train_data/students_pref_{n}.csv', index_col='student_id')
-    # utils_df = pd.read_csv(f'data/train_data/util_{n}.csv', index_col='student_id')
-    pairs_df = pd.read_csv(f'data/train_data/pairs_{n}.csv', index_col=False, na_filter=True)
-    new_df = pd.read_csv(f'data/train_data/preferences_{n}.csv', index_col='student_id')
-    new_df.columns = new_df.columns.astype(int)
-    welfare_1 = task_one(new_df, grades_df)
-    welfare_2 = task_two(new_df, grades_df, pairs_df)
-    print(welfare_1)
-    print(welfare_2)
-    # market_clearing(pref_df, grades_df, utils_df)
-
-
-if __name__ == '__main__':
-    for i in range(1, 7):
-        main(i)
-    main('test')
-    # main(5)
+        if student.is_free():
+            print('Error! missing matching')
+            sys.exit(sid)
+        total_welfare += student.get_utility()
+    # return 73 if 'single' in matching_file else 63
+    return total_welfare
